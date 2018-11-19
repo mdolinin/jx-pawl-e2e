def3c1a4c85d09bb2964a2a5d56e4b028d797ce1
mdolinin <mdolinin@chewy.com>
2018-11-19 11:08:22 -0500

    Add more verification

--- src/test/scala/mdolinin/github/io/demo/SimpleE2ESpec.scala
+++ src/test/scala/mdolinin/github/io/demo/SimpleE2ESpec.scala
@@ -7,6 +7,7 @@ class SimpleE2ESpec extends FlatSpec with WebSpec {
   "JX Demo App" should "show demo page with data from other services" in {
     Guest open "http://jx-react-demo-app.jx-production.35.228.124.40.nip.io"
     Then see "Welcome to Jenkins X with React" in ".App-title" by css
+    And see "To get started, edit src/components/App.js and save to reload." in ".App-intro" by css
     And see "Hello World" in "sacala-service-data"
   }
 }
